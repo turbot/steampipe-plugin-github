64f68b187c00f088cb97b01de951f0ca593e238a
johnsmyth <john@turbot.com>
2021-01-21 19:31:15 -0600

    add table and column descriptions (#3)

--- github/table_github_team.go
+++ github/table_github_team.go
@@ -15,7 +15,7 @@ import (
 func tableGitHubTeam() *plugin.Table {
 	return &plugin.Table{
 		Name:        "github_team",
-		Description: "Github Team",
+		Description: "Github Teams are groups of organization members that reflect your company or group's structure with cascading access permissions and mentions.",
 		List: &plugin.ListConfig{
 			Hydrate: tableGitHubTeamList,
 		},
@@ -26,29 +26,29 @@ func tableGitHubTeam() *plugin.Table {
 		Columns: []*plugin.Column{
 
 			// Top columns
-			{Name: "organization", Type: pb.ColumnType_STRING, Transform: transform.FromField("Organization.Login"), Hydrate: tableGitHubTeamGet},
-			{Name: "slug", Type: pb.ColumnType_STRING},
-			{Name: "name", Type: pb.ColumnType_STRING},
-			{Name: "members_count", Type: pb.ColumnType_INT, Hydrate: tableGitHubTeamGet},
+			{Name: "organization", Type: pb.ColumnType_STRING, Description: "The organization the team is associated with.", Transform: transform.FromField("Organization.Login"), Hydrate: tableGitHubTeamGet},
+			{Name: "slug", Type: pb.ColumnType_STRING, Description: "The team slug name."},
+			{Name: "name", Type: pb.ColumnType_STRING, Description: "The name of the team."},
+			{Name: "members_count", Type: pb.ColumnType_INT, Description: "The number of members.", Hydrate: tableGitHubTeamGet},
 
 			// Not yet supported by go-github
 			//{Name: "created_at", Type: pb.ColumnType_TIMESTAMP, Hydrate: tableGitHubTeamGet, Transform: transform.Transform(convertTimestamp)},
-			{Name: "description", Type: pb.ColumnType_STRING},
-			{Name: "html_url", Type: pb.ColumnType_STRING},
-			{Name: "id", Type: pb.ColumnType_INT},
-			{Name: "members_url", Type: pb.ColumnType_STRING},
-			{Name: "node_id", Type: pb.ColumnType_STRING},
+			{Name: "description", Type: pb.ColumnType_STRING, Description: "The description of the team."},
+			{Name: "html_url", Type: pb.ColumnType_STRING, Description: "The URL of the team page in GitHub."},
+			{Name: "id", Type: pb.ColumnType_INT, Description: "The ID of the team."},
+			{Name: "members_url", Type: pb.ColumnType_STRING, Description: "The API Members URL."},
+			{Name: "node_id", Type: pb.ColumnType_STRING, Description: "The node id of the team."},
 			// Only load relevant fields from the organization
-			{Name: "organization_id", Type: pb.ColumnType_INT, Transform: transform.FromField("Organization.ID"), Hydrate: tableGitHubTeamGet},
-			{Name: "organization_login", Type: pb.ColumnType_STRING, Transform: transform.FromField("Organization.Login"), Hydrate: tableGitHubTeamGet},
-			{Name: "organization_type", Type: pb.ColumnType_STRING, Transform: transform.FromField("Organization.Type"), Hydrate: tableGitHubTeamGet},
-			{Name: "permission", Type: pb.ColumnType_STRING},
-			{Name: "privacy", Type: pb.ColumnType_STRING},
-			{Name: "repos_count", Type: pb.ColumnType_INT, Hydrate: tableGitHubTeamGet},
-			{Name: "repositories_url", Type: pb.ColumnType_STRING},
+			{Name: "organization_id", Type: pb.ColumnType_INT, Description: "The user id (number) of the organization.", Transform: transform.FromField("Organization.ID"), Hydrate: tableGitHubTeamGet},
+			{Name: "organization_login", Type: pb.ColumnType_STRING, Description: "The login name of the organization.", Transform: transform.FromField("Organization.Login"), Hydrate: tableGitHubTeamGet},
+			{Name: "organization_type", Type: pb.ColumnType_STRING, Description: "The type of the organization).", Transform: transform.FromField("Organization.Type"), Hydrate: tableGitHubTeamGet},
+			{Name: "permission", Type: pb.ColumnType_STRING, Description: "The default repository permissions of the team."},
+			{Name: "privacy", Type: pb.ColumnType_STRING, Description: "The privacy setting of the team (closed or secret)."},
+			{Name: "repos_count", Type: pb.ColumnType_INT, Description: "The number of repositories for the team.", Hydrate: tableGitHubTeamGet},
+			{Name: "repositories_url", Type: pb.ColumnType_STRING, Description: "The API Repositories URL."},
 			// Not yet supported by go-github
 			// {Name: "updated_at", Type: pb.ColumnType_TIMESTAMP, Hydrate: tableGitHubTeamGet},
-			{Name: "url", Type: pb.ColumnType_STRING},
+			{Name: "url", Type: pb.ColumnType_STRING, Description: "The API URL of the team."},
 		},
 	}
 }
