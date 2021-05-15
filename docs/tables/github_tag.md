b5e5f7d840f275bccc3e364a67985b5668058370
Nathan Wallace <nathan@turbot.com>
2021-05-15 14:51:56 -0400

    Add table github_tag closes #30

---
+++ docs/tables/github_tag.md
@@ -0,0 +1,57 @@
+# Table: github_tag
+
+Tags mark specific commits in a repository history.
+
+The `github_tag` table can be used to query information about any tag, and **you must specify which repository** in the where or join clause using the `repository_full_name` column.
+
+## Examples
+
+### List tags
+
+```sql
+select
+  name,
+  commit_sha
+from
+  github_tag
+where
+  repository_full_name = 'turbot/steampipe'
+```
+
+### Order tags by semantic version
+
+```sql
+select
+  name,
+  commit_sha
+from
+  github_tag
+where
+  repository_full_name = 'turbot/steampipe'
+order by
+  string_to_array(regexp_replace(name, '[^0-9\.]', '', 'g'), '.')::int[],
+  name;
+```
+
+### Get commit details for each tag (Not working yet)
+
+Note: This example is intended to join tags with commit information to return the
+full details of each item. Currently joins with multiple columns are not
+working pending a solution to [#47](https://github.com/turbot/steampipe-postgres-fdw/issues/47).
+
+```sql
+select
+  t.name,
+  t.commit_sha,
+  c.author_date,
+  c.message
+from
+  github_tag as t,
+  github_commit as c
+where
+  t.repository_full_name = 'turbot/steampipe'
+  and t.repository_full_name = c.repository_full_name
+  and t.commit_sha = c.sha
+order by
+  c.author_date desc;
+```
---
+++ github/table_github_tag.go
@@ -0,0 +1,70 @@
+package github
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v33/github"
+	"github.com/sethvargo/go-retry"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+func tableGitHubTag(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "github_tag",
+		Description: "Tags for commits in the given repository.",
+		List: &plugin.ListConfig{
+			KeyColumns: plugin.SingleColumn("repository_full_name"),
+			Hydrate:    tableGitHubTagList,
+		},
+		Columns: []*plugin.Column{
+			// Top columns
+			{Name: "repository_full_name", Type: proto.ColumnType_STRING, Hydrate: repositoryFullNameQual, Transform: transform.FromValue(), Description: "Full name of the repository that contains the tag."},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the tag."},
+			{Name: "commit_sha", Type: proto.ColumnType_STRING, Transform: transform.FromField("Commit.SHA"), Description: "Commit SHA the tag refers to."},
+			{Name: "commit_url", Type: proto.ColumnType_STRING, Transform: transform.FromField("Commit.URL"), Description: "Commit URL the tag refers to."},
+			{Name: "zipball_url", Type: proto.ColumnType_STRING, Description: "URL to download a zip file of the code for this tag."},
+			{Name: "tarball_url", Type: proto.ColumnType_STRING, Description: "URL to download a tar file of the code for this tag."},
+		},
+	}
+}
+
+func tableGitHubTagList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	client := connect(ctx, d)
+	fullName := d.KeyColumnQuals["repository_full_name"].GetStringValue()
+	s := strings.Split(fullName, "/")
+	owner := s[0]
+	repo := s[1]
+	opts := &github.ListOptions{PerPage: 100}
+	for {
+		var tags []*github.RepositoryTag
+		var resp *github.Response
+		b, err := retry.NewFibonacci(100 * time.Millisecond)
+		if err != nil {
+			return nil, err
+		}
+		err = retry.Do(ctx, retry.WithMaxRetries(10, b), func(ctx context.Context) error {
+			var err error
+			tags, resp, err = client.Repositories.ListTags(ctx, owner, repo, opts)
+			if _, ok := err.(*github.RateLimitError); ok {
+				return retry.RetryableError(err)
+			}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+		for _, i := range tags {
+			d.StreamListItem(ctx, i)
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+	return nil, nil
+}
