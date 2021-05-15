ab8654e869ca92cb3b7acafee514101430198fa8
Nathan Wallace <nathan@turbot.com>
2021-05-15 14:52:43 -0400

    Add table github_traffic_view_daily closes #32

---
+++ github/table_github_traffic_view_daily.go
@@ -0,0 +1,62 @@
+package github
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v33/github"
+	"github.com/sethvargo/go-retry"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+func tableGitHubTrafficViewDaily(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "github_traffic_view_daily",
+		Description: "Daily traffic view over the last 14 days for the given repository.",
+		List: &plugin.ListConfig{
+			KeyColumns: plugin.SingleColumn("repository_full_name"),
+			Hydrate:    tableGitHubTrafficViewDailyList,
+		},
+		Columns: []*plugin.Column{
+			// Top columns
+			{Name: "repository_full_name", Type: proto.ColumnType_STRING, Hydrate: repositoryFullNameQual, Transform: transform.FromValue(), Description: "Full name of the repository that contains the branch."},
+			{Name: "timestamp", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Timestamp").Transform(convertTimestamp), Description: "Date for the view data."},
+			{Name: "count", Type: proto.ColumnType_INT, Description: "View count for the day."},
+			{Name: "uniques", Type: proto.ColumnType_INT, Description: "Unique viewer count for the day."},
+		},
+	}
+}
+
+func tableGitHubTrafficViewDailyList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	client := connect(ctx, d)
+	fullName := d.KeyColumnQuals["repository_full_name"].GetStringValue()
+	s := strings.Split(fullName, "/")
+	owner := s[0]
+	repo := s[1]
+	opts := &github.TrafficBreakdownOptions{Per: "day"}
+	var result *github.TrafficViews
+	var resp *github.Response
+	b, err := retry.NewFibonacci(100 * time.Millisecond)
+	if err != nil {
+		return nil, err
+	}
+	err = retry.Do(ctx, retry.WithMaxRetries(10, b), func(ctx context.Context) error {
+		var err error
+		result, resp, err = client.Repositories.ListTrafficViews(ctx, owner, repo, opts)
+		if _, ok := err.(*github.RateLimitError); ok {
+			return retry.RetryableError(err)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	for _, i := range result.Views {
+		d.StreamListItem(ctx, i)
+	}
+	return nil, nil
+}
