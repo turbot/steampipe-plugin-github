bbcf0db8a0fb07eaacac27aece9e8539f7c94835
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-28 19:49:32 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#156)

--- github/table_github_license.go
+++ github/table_github_license.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/google/go-github/v33/github"
 
-	pb "github.com/turbot/steampipe-plugin-sdk/v2/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v2/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v2/plugin/transform"
+	pb "github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 //// TABLE DEFINITION
--- github/table_github_my_issue.go
+++ github/table_github_my_issue.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/google/go-github/v33/github"
-	"github.com/turbot/steampipe-plugin-sdk/v2/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 //// TABLE DEFINITION
