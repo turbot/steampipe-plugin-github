5cd9e232fd5769080b58e15344e45b76ab6710e5
Jelmer Borst <jelmer.borst@teampicnic.com>
2022-06-10 17:07:27 +0200

    Revert table_github_user.go changes

--- github/table_github_user.go
+++ github/table_github_user.go
@@ -10,42 +10,6 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
-func gitHubUserColumns() []*plugin.Column {
-	return []*plugin.Column{
-		// Top columns
-		{Name: "login", Type: proto.ColumnType_STRING, Description: "The login name of the user."},
-		{Name: "id", Type: proto.ColumnType_INT, Description: "The ID of the user."},
-		{Name: "name", Type: proto.ColumnType_STRING, Description: "The name of the user."},
-		{Name: "type", Type: proto.ColumnType_STRING, Description: "The type of account."},
-		{Name: "node_id", Type: proto.ColumnType_STRING, Description: "The node ID of the user."},
-		{Name: "avatar_url", Type: proto.ColumnType_STRING, Description: "The URL of the user's avatar."},
-		{Name: "html_url", Type: proto.ColumnType_STRING, Description: "The GitHub page for the user."},
-		{Name: "gravatar_id", Type: proto.ColumnType_STRING, Description: "The user's gravatar ID."},
-		{Name: "company", Type: proto.ColumnType_STRING, Description: "The company the user works for."},
-		{Name: "blog", Type: proto.ColumnType_STRING, Description: "The blog address of the user."},
-		{Name: "location", Type: proto.ColumnType_STRING, Description: "The geographic location of the user."},
-		{Name: "email", Type: proto.ColumnType_STRING, Description: "The public email address of the user."},
-		{Name: "hireable", Type: proto.ColumnType_BOOL, Description: "Whether the user currently hireable."},
-		{Name: "bio", Type: proto.ColumnType_STRING, Description: "The biography of the user."},
-		{Name: "twitter_username", Type: proto.ColumnType_STRING, Description: "The twitter username of the user."},
-		{Name: "public_repos", Type: proto.ColumnType_INT, Description: "The number of public repositories owned by the user."},
-		{Name: "public_gists", Type: proto.ColumnType_INT, Description: "The number of public gists owned by the user."},
-		{Name: "followers", Type: proto.ColumnType_INT, Description: "The number of users following the user."},
-		{Name: "following", Type: proto.ColumnType_INT, Description: "The number of users followed by the user."},
-		{Name: "created_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was created.", Transform: transform.FromField("CreatedAt").Transform(convertTimestamp)},
-		{Name: "updated_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was last updated.", Transform: transform.FromField("UpdatedAt").Transform(convertTimestamp)},
-		// {Name: "suspended_at", Type: proto.ColumnType_TIMESTAMP},
-		{Name: "site_admin", Type: proto.ColumnType_BOOL, Description: "If true, user is an administrator."},
-		{Name: "total_private_repos", Type: proto.ColumnType_INT, Description: "The number of private repositories."},
-		{Name: "owned_private_repos", Type: proto.ColumnType_INT, Description: "The number of owned private repositories."},
-		{Name: "private_gists", Type: proto.ColumnType_INT, Description: "The number of private gists owned by the user."},
-		{Name: "disk_usage", Type: proto.ColumnType_INT, Description: "The total disk usage for the user."},
-		{Name: "collaborators", Type: proto.ColumnType_INT, Description: "The number of collaborators."},
-		{Name: "two_factor_authentication", Type: proto.ColumnType_BOOL, Description: "If true, two-factor authentication is enabled."},
-		{Name: "ldap_dn", Type: proto.ColumnType_STRING, Description: "The LDAP distinguished name of the user."},
-	}
-}
-
 //// TABLE DEFINITION
 
 func tableGitHubUser() *plugin.Table {
@@ -57,7 +21,39 @@ func tableGitHubUser() *plugin.Table {
 			ShouldIgnoreError: isNotFoundError([]string{"404"}),
 			Hydrate:           tableGitHubUserGet,
 		},
-		Columns: gitHubUserColumns(),
+		Columns: []*plugin.Column{
+			// Top columns
+			{Name: "login", Type: proto.ColumnType_STRING, Description: "The login name of the user."},
+			{Name: "id", Type: proto.ColumnType_INT, Description: "The ID of the user."},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "The name of the user."},
+			{Name: "type", Type: proto.ColumnType_STRING, Description: "The type of account."},
+			{Name: "node_id", Type: proto.ColumnType_STRING, Description: "The node ID of the user."},
+			{Name: "avatar_url", Type: proto.ColumnType_STRING, Description: "The URL of the user's avatar"},
+			{Name: "html_url", Type: proto.ColumnType_STRING, Description: "The GitHub page for the user."},
+			{Name: "gravatar_id", Type: proto.ColumnType_STRING, Description: "The user's gravatar ID"},
+			{Name: "company", Type: proto.ColumnType_STRING, Description: "The company the user works for."},
+			{Name: "blog", Type: proto.ColumnType_STRING, Description: "The blog address of the user."},
+			{Name: "location", Type: proto.ColumnType_STRING, Description: "The geographic location of the user."},
+			{Name: "email", Type: proto.ColumnType_STRING, Description: "The public email address of the user."},
+			{Name: "hireable", Type: proto.ColumnType_BOOL, Description: "Whether the user currently hireable."},
+			{Name: "bio", Type: proto.ColumnType_STRING, Description: "The biography of the user."},
+			{Name: "twitter_username", Type: proto.ColumnType_STRING, Description: "The twitter username of the user."},
+			{Name: "public_repos", Type: proto.ColumnType_INT, Description: "The number of public repositories owned by the user."},
+			{Name: "public_gists", Type: proto.ColumnType_INT, Description: "The number of public gists owned by the user."},
+			{Name: "followers", Type: proto.ColumnType_INT, Description: "The number of users following the user."},
+			{Name: "following", Type: proto.ColumnType_INT, Description: "The number of users followed by the user."},
+			{Name: "created_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was created.", Transform: transform.FromField("CreatedAt").Transform(convertTimestamp)},
+			{Name: "updated_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was last updated.", Transform: transform.FromField("UpdatedAt").Transform(convertTimestamp)},
+			//{Name: "suspended_at", Type: proto.ColumnType_TIMESTAMP},
+			{Name: "site_admin", Type: proto.ColumnType_BOOL, Description: "If true, user is an administrator."},
+			{Name: "total_private_repos", Type: proto.ColumnType_INT, Description: "The number of private repositories."},
+			{Name: "owned_private_repos", Type: proto.ColumnType_INT, Description: "The number of owned private repositories."},
+			{Name: "private_gists", Type: proto.ColumnType_INT, Description: "The number of private gists owned by the user."},
+			{Name: "disk_usage", Type: proto.ColumnType_INT, Description: "The total disk usage for the user."},
+			{Name: "collaborators", Type: proto.ColumnType_INT, Description: "The number of collaborators."},
+			{Name: "two_factor_authentication", Type: proto.ColumnType_BOOL, Description: "If true, two-factor authentication is enabled."},
+			{Name: "ldap_dn", Type: proto.ColumnType_STRING, Description: "The LDAP distinguished name of the user."},
+		},
 	}
 }
 
