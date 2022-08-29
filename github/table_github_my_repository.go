212d6936f0e2599c11c23469e36294ec8a7ac93e
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2022-08-29 13:04:45 +0530

    Add key columns filter in github_my_repository (#121)

--- github/table_github_my_repository.go
+++ github/table_github_my_repository.go
@@ -16,11 +16,9 @@ func tableGitHubMyRepository() *plugin.Table {
 		List: &plugin.ListConfig{
 			Hydrate:           tableGitHubMyRepositoryList,
 			ShouldIgnoreError: isNotFoundError([]string{"404"}),
-			// https://github.com/turbot/steampipe-mod-github-sherlock/issues/14
-			// https://github.com/turbot/steampipe-postgres-fdw/issues/117
-			// KeyColumns: []*plugin.KeyColumn{
-			// 	{Name: "visibility", Require: plugin.Optional},
-			// },
+			KeyColumns: []*plugin.KeyColumn{
+				{Name: "visibility", Require: plugin.Optional},
+			},
 		},
 		Columns: gitHubRepositoryColumns(),
 	}
@@ -34,13 +32,13 @@ func tableGitHubMyRepositoryList(ctx context.Context, d *plugin.QueryData, h *pl
 	opt := &github.RepositoryListOptions{ListOptions: github.ListOptions{PerPage: 100}}
 
 	// Additional filters
-	// if d.KeyColumnQuals["visibility"] != nil {
-	// 	opt.Visibility = d.KeyColumnQuals["visibility"].GetStringValue()
-	// } else {
-	// Will cause a 422 error if 'type' used in the same request as visibility or
-	// affiliation.
-	opt.Type = "all"
-	//}
+	if d.KeyColumnQuals["visibility"] != nil {
+		opt.Visibility = d.KeyColumnQuals["visibility"].GetStringValue()
+	} else {
+		// Will cause a 422 error if 'type' used in the same request as visibility or
+		// affiliation.
+		opt.Type = "all"
+	}
 	type ListPageResponse struct {
 		repo []*github.Repository
 		resp *github.Response
