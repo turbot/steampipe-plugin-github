81306dba8df6ce7c7173461a294ac095b544a689
misraved <ved@turbot.com>
2022-09-07 03:15:20 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- github/table_github_rate_limit.go
+++ github/table_github_rate_limit.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/google/go-github/v45/github"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableGitHubRateLimit(ctx context.Context) *plugin.Table {
