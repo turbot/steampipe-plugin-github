aef7a87340d16e138dd75f4a098f4a1b86f9a20a
Graza <code@graza.io>
2023-05-04 16:03:11 +0100

    feat: updated github_branch to utilise graphql

--- docs/tables/github_branch.md
+++ docs/tables/github_branch.md
@@ -19,25 +19,19 @@ where
   repository_full_name = 'turbot/steampipe';
 ```
 
-### Get commit details for each branch (Not working yet)
-
-Note: This example is intended to join branches with commit information to return the
-full details of each item. Currently joins with multiple columns are not
-working pending a solution to [#47](https://github.com/turbot/steampipe-postgres-fdw/issues/47).
+### Get commit details for each branch
 
 ```sql
 select
-  t.name,
-  t.commit_sha,
-  c.author_date,
-  c.message
+  name,
+  commit_sha,
+  commit_date,
+  commit_author,
+  commit_message
 from
-  github_branch as t,
-  github_commit as c
+  github_branch
 where
-  t.repository_full_name = 'turbot/steampipe'
-  and t.repository_full_name = c.repository_full_name
-  and t.commit_sha = c.sha
+  repository_full_name = 'turbot/steampipe'
 order by
-  c.author_date desc;
+  commit_date desc;
 ```
--- github/table_github_branch.go
+++ github/table_github_branch.go
@@ -2,16 +2,13 @@ package github
 
 import (
 	"context"
+	"github.com/shurcooL/githubv4"
+	"time"
 
-	"github.com/google/go-github/v48/github"
-
-	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
-
-//// TABLE DEFINITION
 
 func tableGitHubBranch(ctx context.Context) *plugin.Table {
 	return &plugin.Table{
@@ -20,7 +17,6 @@ func tableGitHubBranch(ctx context.Context) *plugin.Table {
 		List: &plugin.ListConfig{
 			KeyColumns: []*plugin.KeyColumn{
 				{Name: "repository_full_name", Require: plugin.Required},
-				{Name: "protected", Require: plugin.Optional, Operators: []string{"<>", "="}},
 			},
 			ShouldIgnoreError: isNotFoundError([]string{"404"}),
 			Hydrate:           tableGitHubBranchList,
@@ -28,88 +24,101 @@ func tableGitHubBranch(ctx context.Context) *plugin.Table {
 		Columns: []*plugin.Column{
 			// Top columns
 			{Name: "repository_full_name", Type: proto.ColumnType_STRING, Transform: transform.FromQual("repository_full_name"), Description: "Full name of the repository that contains the branch."},
-			{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the branch."},
-			{Name: "commit_sha", Type: proto.ColumnType_STRING, Transform: transform.FromField("Commit.SHA"), Description: "Commit SHA the branch refers to."},
-			{Name: "commit_url", Type: proto.ColumnType_STRING, Transform: transform.FromField("Commit.URL"), Description: "Commit URL the branch refers to."},
-			{Name: "protected", Type: proto.ColumnType_BOOL, Description: "True if the branch is protected."},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the branch.", Transform: transform.FromField("Node.Name")},
+			{Name: "commit_sha", Type: proto.ColumnType_STRING, Transform: transform.FromField("Node.Target.Commit.Oid"), Description: "Commit SHA the branch refers to."},
+			{Name: "commit_short_sha", Type: proto.ColumnType_STRING, Transform: transform.FromField("Node.Target.Commit.AbbreviatedOid"), Description: "Commit short SHA the branch refers to."},
+			{Name: "commit_date", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Node.Target.Commit.AuthoredDate"), Description: "Date commit was authored."},
+			{Name: "commit_author", Type: proto.ColumnType_STRING, Transform: transform.FromField("Node.Target.Commit.Author.Name"), Description: "Commit author."},
+			{Name: "commit_message", Type: proto.ColumnType_STRING, Transform: transform.FromField("Node.Target.Commit.Message"), Description: "Commit message."},
+			{Name: "commit_url", Type: proto.ColumnType_STRING, Transform: transform.FromField("Node.Target.Commit.Url"), Description: "Commit URL the branch refers to."},
+			{Name: "protected", Type: proto.ColumnType_BOOL, Transform: transform.FromField("Node.BranchProtectionRule.Id").Transform(HasValue), Description: "True if the branch is protected."},
 		},
 	}
 }
 
-//// LIST FUNCTION
+var branchesQuery struct {
+	Repository struct {
+		Refs struct {
+			TotalCount int
+			PageInfo   struct {
+				EndCursor   githubv4.String
+				HasNextPage bool
+			}
+			Edges []struct {
+				Node struct {
+					Name   string
+					Target struct {
+						Commit struct {
+							Oid            string
+							AbbreviatedOid string
+							Url            string
+							AuthoredDate   time.Time
+							Message        string
+							Author         struct {
+								Name string
+							}
+						} `graphql:"... on Commit"`
+					}
+					BranchProtectionRule struct {
+						Id string
+					}
+				}
+			}
+		} `graphql:"refs(refPrefix: \"refs/heads/\", first: $pageSize, after: $cursor)"`
+	} `graphql:"repository(owner: $owner, name: $repo)"`
+}
 
 func tableGitHubBranchList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	client := connect(ctx, d)
+	client := connectV4(ctx, d)
+
 	fullName := d.EqualsQuals["repository_full_name"].GetStringValue()
 	owner, repo := parseRepoFullName(fullName)
 
-	opts := buildGithubBranchListOptions(d.EqualsQuals, d.Quals)
-	opts.ListOptions = github.ListOptions{PerPage: 100}
-
+	pageSize := 100
 	limit := d.QueryContext.Limit
 	if limit != nil {
-		if *limit < int64(opts.ListOptions.PerPage) {
-			opts.ListOptions.PerPage = int(*limit)
+		if *limit < int64(pageSize) {
+			pageSize = int(*limit)
 		}
 	}
 
-	type ListPageResponse struct {
-		branches []*github.Branch
-		resp     *github.Response
+	variables := map[string]interface{}{
+		"owner":    githubv4.String(owner),
+		"repo":     githubv4.String(repo),
+		"pageSize": githubv4.Int(pageSize),
+		"cursor":   (*githubv4.String)(nil),
 	}
 
-	listPage := func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-		branches, resp, err := client.Repositories.ListBranches(ctx, owner, repo, opts)
-		return ListPageResponse{
-			branches: branches,
-			resp:     resp,
-		}, err
-	}
 	for {
-		listPageResponse, err := retryHydrate(ctx, d, h, listPage)
+		err := client.Query(ctx, &branchesQuery, variables)
 		if err != nil {
+			plugin.Logger(ctx).Error("github_branch", "api_error", err)
 			return nil, err
 		}
-		listResponse := listPageResponse.(ListPageResponse)
-		branches := listResponse.branches
-		resp := listResponse.resp
 
-		for _, i := range branches {
-			if i != nil {
-				d.StreamListItem(ctx, i)
-			}
+		for _, branch := range branchesQuery.Repository.Refs.Edges {
+			d.StreamListItem(ctx, branch)
 
 			// Context can be cancelled due to manual cancellation or the limit has been hit
 			if d.RowsRemaining(ctx) == 0 {
 				return nil, nil
 			}
 		}
-		if resp.NextPage == 0 {
+
+		if !branchesQuery.Repository.Refs.PageInfo.HasNextPage {
 			break
 		}
-		opts.ListOptions.Page = resp.NextPage
+		variables["cursor"] = githubv4.NewString(branchesQuery.Repository.Refs.PageInfo.EndCursor)
 	}
+
 	return nil, nil
 }
 
-func buildGithubBranchListOptions(equalQuals plugin.KeyColumnEqualsQualMap, quals plugin.KeyColumnQualMap) *github.BranchListOptions {
-	request := &github.BranchListOptions{}
-
-	if equalQuals["protected"] != nil {
-		request.Protected = types.Bool(equalQuals["protected"].GetBoolValue())
+// Note: if useful to other tables, move to utils.go
+func HasValue(_ context.Context, input *transform.TransformData) (interface{}, error) {
+	if input.Value == nil || input.Value.(string) == "" {
+		return false, nil
 	}
 
-	// Non-Equals Qual Map handling
-	if quals["protected"] != nil {
-		for _, q := range quals["protected"].Quals {
-			value := q.Value.GetBoolValue()
-			if q.Operator == "<>" {
-				request.Protected = types.Bool(false)
-				if !value {
-					request.Protected = types.Bool(true)
-				}
-			}
-		}
-	}
-	return request
+	return true, nil
 }
