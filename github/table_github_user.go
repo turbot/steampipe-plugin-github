aa09bc048af0fe5598d231fa12832c7a372ec646
Graza <code@graza.io>
2023-05-05 23:04:38 +0100

    feat: added extra fields to github_user table

--- github/table_github_user.go
+++ github/table_github_user.go
@@ -48,9 +48,42 @@ func tableGitHubUser() *plugin.Table {
 			{Name: "owned_private_repos", Type: proto.ColumnType_INT, Description: "The number of owned private repositories.", Transform: transform.FromField("PrivateRepos.TotalCount")},
 			{Name: "disk_usage", Type: proto.ColumnType_INT, Description: "The total disk usage for the user.", Transform: transform.FromField("Repositories.TotalDiskUsage")},
 			{Name: "status_message", Type: proto.ColumnType_STRING, Description: "The status message set by the user.", Transform: transform.FromField("Status.Message")},
+			{Name: "status_expires_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the status message expires.", Transform: transform.FromField("Status.ExpiresAt")},
 			{Name: "issues", Type: proto.ColumnType_INT, Description: "Count of issues authored by the user.", Transform: transform.FromField("Issues.TotalCount")},
 			{Name: "organizations", Type: proto.ColumnType_INT, Description: "Count of organizations the user is a member of.", Transform: transform.FromField("Organizations.TotalCount")},
 			{Name: "pronouns", Type: proto.ColumnType_STRING, Description: "The pronouns of the user."},
+			{Name: "any_pinnable_items", Type: proto.ColumnType_BOOL, Description: "If true, user has items that are pinnable."},
+			{Name: "has_sponsors_listing", Type: proto.ColumnType_BOOL, Description: "If true, user has a GitHub sponsors listing."},
+			{Name: "bounty_hunter", Type: proto.ColumnType_BOOL, Description: "If true, user is a participant in the GitHub bounty hunter program.", Transform: transform.FromField("IsBountyHunter")},
+			{Name: "campus_expert", Type: proto.ColumnType_BOOL, Description: "If true, user is a participant in the GitHub campus experts program.", Transform: transform.FromField("IsCampusExpert")},
+			{Name: "developer_program_member", Type: proto.ColumnType_BOOL, Description: "If true, user is a GitHub Developer Program member..", Transform: transform.FromField("IsDeveloperProgramMember")},
+			{Name: "employee", Type: proto.ColumnType_BOOL, Description: "If true, user is a GitHub employee.", Transform: transform.FromField("IsEmployee")},
+			{Name: "github_star", Type: proto.ColumnType_BOOL, Description: "If true, user is a participant in the GitHub Stars program.", Transform: transform.FromField("IsGitHubStar")},
+			{Name: "following_you", Type: proto.ColumnType_BOOL, Description: "If true, user is following you.", Transform: transform.FromField("IsFollowingViewer")},
+			{Name: "sponsoring_you", Type: proto.ColumnType_BOOL, Description: "If true, user is sponsoring you.", Transform: transform.FromField("IsSponsoringViewer")},
+			{Name: "can_change_pinned_items", Type: proto.ColumnType_BOOL, Description: "If true, you can change pinned items on the users profile.", Transform: transform.FromField("ViewerCanChangePinnedItems")},
+			{Name: "can_create_projects", Type: proto.ColumnType_BOOL, Description: "If true, you can create projects in the users account.", Transform: transform.FromField("ViewerCanCreateProjects")},
+			{Name: "can_follow", Type: proto.ColumnType_BOOL, Description: "If true, you can follow this user.", Transform: transform.FromField("ViewerCanFollow")},
+			{Name: "are_following", Type: proto.ColumnType_BOOL, Description: "If true, you are following this user.", Transform: transform.FromField("ViewerIsFollowing")},
+			{Name: "can_sponsor", Type: proto.ColumnType_BOOL, Description: "If true, you can sponsor this user.", Transform: transform.FromField("ViewerCanSponsor")},
+			{Name: "are_sponsoring", Type: proto.ColumnType_BOOL, Description: "If true, you are sponsoring this user.", Transform: transform.FromField("ViewerIsSponsoring")},
+			{Name: "issue_comments", Type: proto.ColumnType_INT, Description: "Count of issue comments made by the user.", Transform: transform.FromField("IssueComments.TotalCount")},
+			{Name: "estimated_next_sponsors_payout_in_cents", Type: proto.ColumnType_INT, Description: "The estimated next GitHub Sponsors payout for this user in cents (USD).", Transform: transform.FromField("EstimatedNextSponsorsPayoutInCents")},
+			{Name: "monthly_estimated_sponsors_income_in_cents", Type: proto.ColumnType_INT, Description: "The estimated monthly GitHub Sponsors income for this user in cents (USD).", Transform: transform.FromField("EstimatedNextSponsorsPayoutInCents")},
+			{Name: "packages", Type: proto.ColumnType_INT, Description: "Count of packages owned by the user.", Transform: transform.FromField("Packages.TotalCount")},
+			{Name: "pinned_items", Type: proto.ColumnType_INT, Description: "Count of items currently pinned to the users profile.", Transform: transform.FromField("PinnedItems.TotalCount")},
+			{Name: "pinned_items_remaining", Type: proto.ColumnType_INT, Description: "Count of available slots to pin items on the users profile.", Transform: transform.FromField("PinnedItemsRemaining")},
+			{Name: "projects", Type: proto.ColumnType_INT, Description: "Count of projects under the user.", Transform: transform.FromField("Projects.TotalCount")},
+			{Name: "projects_url", Type: proto.ColumnType_STRING, Description: "The URL for the users projects.", Transform: transform.FromField("ProjectsUrl")},
+			{Name: "public_keys", Type: proto.ColumnType_INT, Description: "Count of public keys for the user.", Transform: transform.FromField("PublicKeys.TotalCount")},
+			{Name: "open_pull_requests", Type: proto.ColumnType_INT, Description: "Count of open pull requests the user has.", Transform: transform.FromField("OpenPullRequests.TotalCount")},
+			{Name: "merged_pull_requests", Type: proto.ColumnType_INT, Description: "Count of merged pull requests the user has.", Transform: transform.FromField("MergedPullRequests.TotalCount")},
+			{Name: "closed_pull_requests", Type: proto.ColumnType_INT, Description: "Count of closed pull requests the user has.", Transform: transform.FromField("ClosedPullRequests.TotalCount")},
+			{Name: "social_accounts", Type: proto.ColumnType_JSON, Description: "Array of social accounts associated with the user.", Transform: transform.FromField("SocialAccounts.Nodes")},
+			{Name: "sponsoring", Type: proto.ColumnType_INT, Description: "Count of users the user is sponsoring.", Transform: transform.FromField("Sponsoring.TotalCount")},
+			{Name: "sponsors", Type: proto.ColumnType_INT, Description: "Count of sponsors the user has.", Transform: transform.FromField("Sponsors.TotalCount")},
+			{Name: "starred_repositories", Type: proto.ColumnType_INT, Description: "Count of repositories the user has starred.", Transform: transform.FromField("StarredRepositories.TotalCount")},
+			{Name: "watching", Type: proto.ColumnType_INT, Description: "Count of repositories the user is watching.", Transform: transform.FromField("Watching.TotalCount")},
 			// {Name: "private_gists", Type: proto.ColumnType_INT, Description: "The number of private gists owned by the user.", Transform: transform.FromField("PrivateGists.TotalCount")},
 			// {Name: "collaborators", Type: proto.ColumnType_INT, Description: "The number of collaborators."},
 			// {Name: "two_factor_authentication", Type: proto.ColumnType_BOOL, Description: "If true, two-factor authentication is enabled."},
@@ -99,7 +132,8 @@ var userQuery struct {
 		CreatedAt time.Time
 		UpdatedAt time.Time
 		Status    struct {
-			Message string
+			Message   string
+			ExpiresAt time.Time
 		}
 		Issues struct {
 			TotalCount int
@@ -107,7 +141,68 @@ var userQuery struct {
 		Organizations struct {
 			TotalCount int
 		}
-		Pronouns string
+		Pronouns                   string
+		AnyPinnableItems           bool
+		HasSponsorsListing         bool
+		IsBountyHunter             bool
+		IsCampusExpert             bool
+		IsDeveloperProgramMember   bool
+		IsEmployee                 bool
+		IsGitHubStar               bool
+		IsFollowingViewer          bool
+		IsSponsoringViewer         bool
+		ViewerCanChangePinnedItems bool
+		ViewerCanCreateProjects    bool
+		ViewerCanFollow            bool
+		ViewerCanSponsor           bool
+		ViewerIsFollowing          bool
+		ViewerIsSponsoring         bool
+		IssueComments              struct {
+			TotalCount int
+		}
+		EstimatedNextSponsorsPayoutInCents    int
+		MonthlyEstimatedSponsorsIncomeInCents int
+		Packages                              struct {
+			TotalCount int
+		}
+		PinnedItems struct {
+			TotalCount int
+		}
+		PinnedItemsRemaining int
+		Projects             struct {
+			TotalCount int
+		}
+		ProjectsUrl string
+		PublicKeys  struct {
+			TotalCount int
+		}
+		OpenPullRequests struct {
+			TotalCount int
+		} `graphql:"openPullRequests: pullRequests(states: OPEN)"`
+		MergedPullRequests struct {
+			TotalCount int
+		} `graphql:"mergedPullRequests: pullRequests(states: MERGED)"`
+		ClosedPullRequests struct {
+			TotalCount int
+		} `graphql:"closedPullRequests: pullRequests(states: CLOSED)"`
+		SocialAccounts struct {
+			Nodes []struct {
+				DisplayName string
+				Provider    string
+			}
+		} `graphql:"socialAccounts(first: 10)"`
+		Sponsoring struct {
+			TotalCount int
+		}
+		Sponsors struct {
+			TotalCount int
+		}
+		StarredRepositories struct {
+			TotalCount int
+		}
+		Watching struct {
+			TotalCount int
+		}
 	} `graphql:"user(login: $login)"`
 }
 
