4a871630f8ee31fb4811b8484dc6391bbb29533f
Graza <code@graza.io>
2023-05-12 15:19:31 +0100

    refactor: user table, graphql, default columns

--- github/table_github_user.go
+++ github/table_github_user.go
@@ -2,6 +2,9 @@ package github
 
 import (
 	"context"
+	"github.com/shurcooL/githubv4"
+	"github.com/turbot/steampipe-plugin-github/github/models"
+	"strings"
 
 	"github.com/google/go-github/v48/github"
 
@@ -9,8 +12,6 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
-
-//// TABLE DEFINITION
 
 func tableGitHubUser() *plugin.Table {
 	return &plugin.Table{
@@ -22,89 +23,85 @@ func tableGitHubUser() *plugin.Table {
 			Hydrate:           tableGitHubUserGet,
 		},
 		Columns: []*plugin.Column{
-			// Top columns
 			{Name: "login", Type: proto.ColumnType_STRING, Description: "The login name of the user."},
-			{Name: "id", Type: proto.ColumnType_INT, Description: "The ID of the user."},
-			{Name: "name", Type: proto.ColumnType_STRING, Description: "The name of the user."},
-			{Name: "type", Type: proto.ColumnType_STRING, Description: "The type of account."},
-			{Name: "node_id", Type: proto.ColumnType_STRING, Description: "The node ID of the user."},
-			{Name: "avatar_url", Type: proto.ColumnType_STRING, Description: "The URL of the user's avatar"},
-			{Name: "html_url", Type: proto.ColumnType_STRING, Description: "The GitHub page for the user."},
-			{Name: "gravatar_id", Type: proto.ColumnType_STRING, Description: "The user's gravatar ID"},
-			{Name: "company", Type: proto.ColumnType_STRING, Description: "The company the user works for."},
-			{Name: "blog", Type: proto.ColumnType_STRING, Description: "The blog address of the user."},
-			{Name: "location", Type: proto.ColumnType_STRING, Description: "The geographic location of the user."},
-			{Name: "email", Type: proto.ColumnType_STRING, Description: "The public email address of the user."},
-			{Name: "hireable", Type: proto.ColumnType_BOOL, Description: "Whether the user currently hireable."},
+			{Name: "id", Type: proto.ColumnType_INT, Description: "The ID of the user.", Transform: transform.FromField("Id")},
+			{Name: "node_id", Type: proto.ColumnType_STRING, Description: "The node ID of the user.", Transform: transform.FromField("NodeId")},
+			{Name: "email", Type: proto.ColumnType_STRING, Description: "The email of the user."},
+			{Name: "url", Type: proto.ColumnType_STRING, Description: "The URL of the user's GitHub page.", Transform: transform.FromField("Url")},
+			{Name: "created_at", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when user was created."},
+			{Name: "updated_at", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when user was last updated."},
+			{Name: "any_pinnable_items", Type: proto.ColumnType_BOOL, Description: "If true, user has pinnable items."},
+			{Name: "avatar_url", Type: proto.ColumnType_STRING, Description: "The URL of the user's avatar.", Transform: transform.FromField("AvatarUrl")},
 			{Name: "bio", Type: proto.ColumnType_STRING, Description: "The biography of the user."},
-			{Name: "twitter_username", Type: proto.ColumnType_STRING, Description: "The twitter username of the user."},
-			{Name: "public_repos", Type: proto.ColumnType_INT, Description: "The number of public repositories owned by the user."},
-			{Name: "public_gists", Type: proto.ColumnType_INT, Description: "The number of public gists owned by the user."},
-			{Name: "followers", Type: proto.ColumnType_INT, Description: "The number of users following the user."},
-			{Name: "following", Type: proto.ColumnType_INT, Description: "The number of users followed by the user."},
-			{Name: "created_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was created.", Transform: transform.FromField("CreatedAt").Transform(convertTimestamp)},
-			{Name: "updated_at", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp when the user was last updated.", Transform: transform.FromField("UpdatedAt").Transform(convertTimestamp)},
-			//{Name: "suspended_at", Type: proto.ColumnType_TIMESTAMP},
-			{Name: "site_admin", Type: proto.ColumnType_BOOL, Description: "If true, user is an administrator."},
-			{Name: "total_private_repos", Type: proto.ColumnType_INT, Description: "The number of private repositories."},
-			{Name: "owned_private_repos", Type: proto.ColumnType_INT, Description: "The number of owned private repositories."},
-			{Name: "private_gists", Type: proto.ColumnType_INT, Description: "The number of private gists owned by the user."},
-			{Name: "disk_usage", Type: proto.ColumnType_INT, Description: "The total disk usage for the user."},
-			{Name: "collaborators", Type: proto.ColumnType_INT, Description: "The number of collaborators."},
-			{Name: "two_factor_authentication", Type: proto.ColumnType_BOOL, Description: "If true, two-factor authentication is enabled."},
-			{Name: "ldap_dn", Type: proto.ColumnType_STRING, Description: "The LDAP distinguished name of the user."},
+			{Name: "company", Type: proto.ColumnType_STRING, Description: "The company on the users profile."},
+			{Name: "estimated_next_sponsors_payout_in_cents", Type: proto.ColumnType_INT, Description: "The estimated next GitHub sponsors payout for this user in cents (USD)."},
+			{Name: "has_sponsors_listing", Type: proto.ColumnType_BOOL, Description: "If true, user has a GitHub sponsors listing."},
+			{Name: "interaction_ability", Type: proto.ColumnType_JSON, Description: "The interaction ability settings for this user.", Transform: transform.FromField("InteractionAbility").NullIfZero()},
+			{Name: "is_bounty_hunter", Type: proto.ColumnType_BOOL, Description: "If true, user is a participant in the GitHub security bug bounty."},
+			{Name: "is_campus_expert", Type: proto.ColumnType_BOOL, Description: "If true, user is a participant in the GitHub campus experts program."},
+			{Name: "is_developer_program_member", Type: proto.ColumnType_BOOL, Description: "If true, user is a GitHub developer program member."},
+			{Name: "is_employee", Type: proto.ColumnType_BOOL, Description: "If true, user is a GitHub employee."},
+			{Name: "is_following_you", Type: proto.ColumnType_BOOL, Description: "If true, user follows you."},
+			{Name: "is_github_star", Type: proto.ColumnType_BOOL, Description: "If true, user is a member of the GitHub Stars Program.", Transform: transform.FromField("IsGitHubStar")},
+			{Name: "is_hireable", Type: proto.ColumnType_BOOL, Description: "If true, user has marked themselves as for hire."},
+			{Name: "is_site_admin", Type: proto.ColumnType_BOOL, Description: "If true, user is a site administrator."},
+			{Name: "is_sponsoring_you", Type: proto.ColumnType_BOOL, Description: "If true, this user is sponsoring you."},
+			{Name: "is_you", Type: proto.ColumnType_BOOL, Description: "If true, user is you."},
+			{Name: "location", Type: proto.ColumnType_STRING, Description: "The location of the user."},
+			{Name: "monthly_estimated_sponsors_income_in_cents", Type: proto.ColumnType_INT, Description: "The estimated monthly GitHub sponsors income for this user in cents (USD)."},
+			{Name: "pinned_items_remaining", Type: proto.ColumnType_INT, Description: "How many more items this user can pin to their profile."},
+			{Name: "projects_url", Type: proto.ColumnType_STRING, Description: "The URL listing user's projects.", Transform: transform.FromField("ProjectsUrl")},
+			{Name: "pronouns", Type: proto.ColumnType_STRING, Description: "The user's pronouns."},
+			{Name: "sponsors_listing", Type: proto.ColumnType_JSON, Description: "The user's sponsors listing.", Transform: transform.FromField("SponsorsListing").NullIfZero()},
+			{Name: "status", Type: proto.ColumnType_JSON, Description: "The user's status.", Transform: transform.FromField("Status").NullIfZero()},
+			{Name: "twitter_username", Type: proto.ColumnType_STRING, Description: "Twitter username of the user."},
+			{Name: "can_changed_pinned_items", Type: proto.ColumnType_BOOL, Description: "If true, you can change the pinned items for this user."},
+			{Name: "can_create_projects", Type: proto.ColumnType_BOOL, Description: "If true, you can create projects for this user."},
+			{Name: "can_follow", Type: proto.ColumnType_BOOL, Description: "If true, you can follow this user."},
+			{Name: "can_sponsor", Type: proto.ColumnType_BOOL, Description: "If true, you can sponsor this user."},
+			{Name: "is_following", Type: proto.ColumnType_BOOL, Description: "If true, you are following this user."},
+			{Name: "is_sponsoring", Type: proto.ColumnType_BOOL, Description: "If true, you are sponsoring this user."},
+			{Name: "website_url", Type: proto.ColumnType_STRING, Description: "The URL pointing to the user's public website/blog.", Transform: transform.FromField("WebsiteUrl")},
 		},
 	}
 }
 
-//// HYDRATE FUNCTRIONS
-
-// Listing all users is not terribly useful, so we require a 'login' qual and essentially always
-// do a 'get':  from GitHub API docs: https://developer.github.com/v3/users/#list-users:
-//
-//	    	Lists all users, in the order that they signed up on GitHub. This list includes personal user
-//			accounts and organization accounts.
-//	    	Note: Pagination is powered exclusively by the since parameter. Use the Link header to get
-//			the URL for the next page of users.
 func tableGitHubUserGet(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	logger := plugin.Logger(ctx)
-
 	var login string
-
 	if h.Item != nil {
 		item := h.Item.(*github.User)
-		logger.Trace("tableGitHubUserGet", item.String())
+		plugin.Logger(ctx).Trace("tableGitHubUserGet", item.String())
 		login = *item.Login
 	} else {
 		login = d.EqualsQuals["login"].GetStringValue()
 	}
 
-	client := connect(ctx, d)
-
-	type GetResponse struct {
-		user *github.User
-		resp *github.Response
+	if login == "" {
+		return nil, nil
 	}
 
-	getDetails := func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-		detail, resp, err := client.Users.Get(ctx, login)
-		return GetResponse{
-			user: detail,
-			resp: resp,
-		}, err
-	}
-	getResponse, err := retryHydrate(ctx, d, h, getDetails)
+	client := connectV4(ctx, d)
 
+	var query struct {
+		RateLimit models.RateLimit
+		User      models.User `graphql:"user(login: $login)"`
+	}
+
+	variables := map[string]interface{}{
+		"login": githubv4.String(login),
+	}
+
+	err := client.Query(ctx, &query, variables)
+	plugin.Logger(ctx).Debug(rateLimitLogString("github_user", &query.RateLimit))
 	if err != nil {
+		plugin.Logger(ctx).Error("github_user", "api_error", err)
+		if strings.Contains(err.Error(), "Could not resolve to a User with the login of") {
+			return nil, nil
+		}
 		return nil, err
 	}
 
-	getResp := getResponse.(GetResponse)
-	user := getResp.user
-
-	if user != nil {
-		d.StreamListItem(ctx, user)
-	}
+	d.StreamListItem(ctx, query.User)
 
 	return nil, nil
 }
