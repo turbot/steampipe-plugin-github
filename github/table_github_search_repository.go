d76848f658050610cd5efaa3fee021e32e1483dd
Graza <code@graza.io>
2023-05-18 19:27:48 +0100

    refactor(wip): repo & user search tables

--- github/table_github_search_repository.go
+++ github/table_github_search_repository.go
@@ -2,14 +2,16 @@ package github
 
 import (
 	"context"
+	"github.com/shurcooL/githubv4"
+	"github.com/turbot/steampipe-plugin-github/github/models"
 
 	"github.com/google/go-github/v48/github"
-	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
-//// TABLE DEFINITION
+func gitHubSearchRepositoryColumns() []*plugin.Column {
+	return append(defaultSearchColumns(), sharedRepositoryColumns()...)
+}
 
 func tableGitHubSearchRepository(ctx context.Context) *plugin.Table {
 	return &plugin.Table{
@@ -19,16 +21,67 @@ func tableGitHubSearchRepository(ctx context.Context) *plugin.Table {
 			KeyColumns: plugin.SingleColumn("query"),
 			Hydrate:    tableGitHubSearchRepositoryList,
 		},
-		Columns: gitHubSearchRepositoryColumns([]*plugin.Column{
-			{Name: "query", Type: proto.ColumnType_STRING, Transform: transform.FromQual("query"), Description: "The query used to match the repository."},
-			{Name: "text_matches", Type: proto.ColumnType_JSON, Description: "The text match details."},
-		}),
+		Columns: gitHubSearchRepositoryColumns(),
 	}
 }
 
-//// LIST FUNCTION
-
 func tableGitHubSearchRepositoryList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	quals := d.EqualsQuals
+	input := quals["query"].GetStringValue()
+
+	if input == "" {
+		return nil, nil
+	}
+
+	var query struct {
+		RateLimit models.RateLimit
+		Search    struct {
+			RepositoryCount int
+			PageInfo        models.PageInfo
+			Edges           []struct {
+				TextMatches []models.TextMatch
+				Node        struct {
+					models.Repository `graphql:"... on Repository"`
+				}
+			}
+		} `graphql:"search(type: REPOSITORY, first: $pageSize, after: $cursor, query: $query)"`
+	}
+
+	pageSize := adjustPageSize(75, d.QueryContext.Limit)
+	variables := map[string]interface{}{
+		"pageSize": githubv4.Int(pageSize),
+		"cursor":   (*githubv4.String)(nil),
+		"query":    githubv4.String(input),
+	}
+
+	client := connectV4(ctx, d)
+	for {
+		err := client.Query(ctx, &query, variables)
+		plugin.Logger(ctx).Debug(rateLimitLogString("github_search_repository", &query.RateLimit))
+		if err != nil {
+			plugin.Logger(ctx).Error("github_search_repository", "api_error", err)
+			return nil, err
+		}
+
+		for _, repo := range query.Search.Edges {
+			d.StreamListItem(ctx, repo)
+
+			// Context can be cancelled due to manual cancellation or the limit has been hit
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+
+		if !query.Search.PageInfo.HasNextPage {
+			break
+		}
+		variables["cursor"] = githubv4.NewString(query.Search.PageInfo.EndCursor)
+	}
+
+	return nil, nil
+}
+
+func tableGitHubSearchRepositoryListOld(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	logger := plugin.Logger(ctx)
 	logger.Trace("tableGitHubSearchRepositoryList")
 
