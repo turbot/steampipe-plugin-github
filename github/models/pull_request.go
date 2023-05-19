ae907c7ce6b4488dfd7534977aa16d855d704b12
Graza <code@graza.io>
2023-05-19 11:33:04 +0100

    refactor: reworked pullrequest structs

--- github/models/pull_request.go
+++ github/models/pull_request.go
@@ -2,51 +2,58 @@ package models
 
 import "github.com/shurcooL/githubv4"
 
+type BasicPullRequest struct {
+	Id                  int                                `graphql:"id: databaseId" json:"id"`
+	NodeId              string                             `graphql:"nodeId: id" json:"node_id"`
+	Number              int                                `json:"number"`
+	ActiveLockReason    githubv4.LockReason                `json:"active_lock_reason"`
+	Additions           int                                `json:"additions"`
+	Author              Actor                              `json:"author"`
+	AuthorAssociation   githubv4.CommentAuthorAssociation  `json:"author_association"`
+	BaseRefName         string                             `json:"base_ref_name"`
+	Body                string                             `json:"body"`
+	ChangedFiles        int                                `json:"changed_files"`
+	ChecksUrl           string                             `json:"checks_url"`
+	Closed              bool                               `json:"closed"`
+	ClosedAt            NullableTime                       `json:"closed_at"`
+	CreatedAt           NullableTime                       `json:"created_at"`
+	CreatedViaEmail     bool                               `json:"created_via_email"`
+	Deletions           int                                `json:"deletions"`
+	Editor              Actor                              `json:"editor"`
+	HeadRefName         string                             `json:"head_ref_name"`
+	HeadRefOid          string                             `json:"head_ref_oid"`
+	IncludesCreatedEdit bool                               `json:"includes_created_edit"`
+	IsCrossRepository   bool                               `json:"is_cross_repository"`
+	IsDraft             bool                               `json:"is_draft"`
+	IsReadByUser        bool                               `graphql:"isReadByUser: isReadByViewer" json:"is_read_by_user"`
+	LastEditedAt        NullableTime                       `json:"last_edited_at"`
+	Locked              bool                               `json:"locked"`
+	MaintainerCanModify bool                               `json:"maintainer_can_modify"`
+	Mergeable           githubv4.MergeableState            `json:"mergeable"`
+	Merged              bool                               `json:"merged"`
+	MergedAt            NullableTime                       `json:"merged_at"`
+	MergedBy            Actor                              `json:"merged_by"`
+	Milestone           Milestone                          `json:"milestone"`
+	Permalink           string                             `json:"permalink"`
+	PublishedAt         NullableTime                       `json:"published_at"`
+	RevertUrl           string                             `json:"revert_url"`
+	ReviewDecision      githubv4.PullRequestReviewDecision `json:"review_decision"`
+	State               githubv4.PullRequestState          `json:"state"`
+	Title               string                             `json:"title"`
+	TotalCommentsCount  int                                `json:"total_comments_count"`
+	UpdatedAt           NullableTime                       `json:"updated_at"`
+	Url                 string                             `json:"url"`
+	Repo                struct {
+		NameWithOwner string `json:"name_with_owner"`
+	} `graphql:"repo: repository" json:"repo"`
+}
+
 type PullRequest struct {
-	Id                  int                                  `graphql:"id: databaseId" json:"id"`
-	NodeId              string                               `graphql:"nodeId: id" json:"node_id"`
-	Number              int                                  `json:"number"`
-	ActiveLockReason    githubv4.LockReason                  `json:"active_lock_reason"`
-	Additions           int                                  `json:"additions"`
-	Author              Actor                                `json:"author"`
-	AuthorAssociation   githubv4.CommentAuthorAssociation    `json:"author_association"`
+	BasicPullRequest
 	BaseRef             BasicRef                             `json:"base_ref"`
-	BaseRefName         string                               `json:"base_ref_name"`
-	Body                string                               `json:"body"`
-	ChangedFiles        int                                  `json:"changed_files"`
-	ChecksUrl           string                               `json:"checks_url"`
-	Closed              bool                                 `json:"closed"`
-	ClosedAt            NullableTime                         `json:"closed_at"`
-	CreatedAt           NullableTime                         `json:"created_at"`
-	CreatedViaEmail     bool                                 `json:"created_via_email"`
-	Deletions           int                                  `json:"deletions"`
-	Editor              Actor                                `json:"editor"`
 	HeadRef             BasicRef                             `json:"head_ref"`
-	HeadRefName         string                               `json:"head_ref_name"`
-	HeadRefOid          string                               `json:"head_ref_oid"`
-	IncludesCreatedEdit bool                                 `json:"includes_created_edit"`
-	IsCrossRepository   bool                                 `json:"is_cross_repository"`
-	IsDraft             bool                                 `json:"is_draft"`
-	IsReadByUser        bool                                 `graphql:"isReadByUser: isReadByViewer" json:"is_read_by_user"`
-	LastEditedAt        NullableTime                         `json:"last_edited_at"`
-	Locked              bool                                 `json:"locked"`
-	MaintainerCanModify bool                                 `json:"maintainer_can_modify"`
 	MergeCommit         Commit                               `json:"merge_commit"`
-	MergeableState      githubv4.MergeableState              `json:"mergeable_state"`
-	Merged              bool                                 `json:"merged"`
-	MergedAt            NullableTime                         `json:"merged_at"`
-	MergedBy            Actor                                `json:"merged_by"`
-	Milestone           Milestone                            `json:"milestone"`
-	Permalink           string                               `json:"permalink"`
-	PublishedAt         NullableTime                         `json:"published_at"`
-	RevertUrl           string                               `json:"revert_url"`
-	ReviewDecision      githubv4.PullRequestReviewDecision   `json:"review_decision"`
-	State               githubv4.PullRequestState            `json:"state"`
 	SuggestedReviewers  []SuggestedReviewer                  `json:"suggested_reviewers"`
-	Title               string                               `json:"title"`
-	TotalCommentsCount  int                                  `json:"total_comments_count"`
-	UpdatedAt           NullableTime                         `json:"updated_at"`
-	Url                 string                               `json:"url"`
 	CanApplySuggestion  bool                                 `graphql:"canApplySuggestion:viewerCanApplySuggestion" json:"can_apply_suggestion"`
 	CanClose            bool                                 `graphql:"canClose:viewerCanClose" json:"can_close"`
 	CanDeleteHeadRef    bool                                 `graphql:"canDeleteHeadRef:viewerCanDeleteHeadRef" json:"can_delete_head_ref"`
@@ -62,9 +69,7 @@ type PullRequest struct {
 	DidAuthor           bool                                 `graphql:"didAuthor:viewerDidAuthor" json:"did_author"`
 	CannotUpdateReasons []githubv4.CommentCannotUpdateReason `graphql:"cannotUpdateReasons: viewerCannotUpdateReasons" json:"cannot_update_reasons"`
 	Subscription        githubv4.SubscriptionState           `graphql:"subscription: viewerSubscription" json:"subscription"`
-	Repo                struct {
-		NameWithOwner string `json:"name_with_owner"`
-	} `graphql:"repo: repository" json:"repo"`
+
 	// Assignees [pageable]
 	// ClosingIssueReferences [pageable]
 	// Comments [pageable]
@@ -83,7 +88,6 @@ type PullRequest struct {
 	// ReviewThreads [pageable]
 	// Reviews [pageable]
 	// TimelineItems [pageable]
-
 }
 
 type SuggestedReviewer struct {
