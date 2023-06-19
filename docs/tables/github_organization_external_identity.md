c6efc8739c4259385497c24fb9aa58db859b2627
Graza <code@graza.io>
2023-06-19 16:12:43 +0100

    feat: organization_external_identity table

---
+++ docs/tables/github_organization_external_identity.md
@@ -0,0 +1,40 @@
+# Table: github_organization_external_identity
+
+The `github_organization_external_identity` used to query information about external identities of an organization.
+
+**You must specify the organization** in the where or join clause (`where organization=`, `join github_organization_external_identity on organization=`).
+
+## Examples
+
+### List external identities of an organization
+
+```sql
+select
+  guid,
+  user ->> 'login' as user_login,
+  saml_identity ->> 'username' as saml_user,
+  scim_identity ->> 'username' as scim_user,
+  organization_invitation ->> 'role' as invited_role
+from
+  github_organization_external_identity
+where
+  organization = 'turbot';
+```
+
+### List external identities for all your organizations
+
+```sql
+select
+  o.login as org,
+  o.saml_identity_provider ->> 'sso_url' as sso_url,
+  e.user ->> 'login' as user_login,
+  e.saml_identity ->> 'username' as saml_user,
+  e.scim_identity ->> 'username' as scim_user,
+  e.organization_invitation ->> 'role' as invited_role
+from
+  github_my_organization o
+join
+  github_organization_external_identity e
+on 
+  o.login = e.organization;
+```
---
+++ github/table_github_organization_external_identity.go
@@ -0,0 +1,95 @@
+package github
+
+import (
+	"context"
+	"github.com/shurcooL/githubv4"
+	"github.com/turbot/steampipe-plugin-github/github/models"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+)
+
+func gitHubOrganizationExternalIdentityColumns() []*plugin.Column {
+	return []*plugin.Column{
+		{Name: "organization", Type: proto.ColumnType_STRING, Description: "The organization the external identity is associated with.", Transform: transform.FromQual("organization")},
+		{Name: "guid", Type: proto.ColumnType_STRING, Description: "Guid identifier for the external identity."},
+		{Name: "user", Type: proto.ColumnType_JSON, Description: "The GitHub user account."},
+		{Name: "saml_identity", Type: proto.ColumnType_JSON, Description: "The external SAML identity."},
+		{Name: "scim_identity", Type: proto.ColumnType_JSON, Description: "The external SCIM identity."},
+		{Name: "organization_invitation", Type: proto.ColumnType_JSON, Description: "The invitation to the organization."},
+	}
+}
+
+func tableGitHubOrganizationExternalIdentity() *plugin.Table {
+	return &plugin.Table{
+		Name:        "github_organization_external_identity",
+		Description: "GitHub members for a given organization. GitHub Users are user accounts in GitHub.",
+		List: &plugin.ListConfig{
+			KeyColumns: []*plugin.KeyColumn{
+				{
+					Name:    "organization",
+					Require: plugin.Required,
+				},
+			},
+			Hydrate: tableGitHubOrganizationExternalIdentityList,
+		},
+		Columns: gitHubOrganizationExternalIdentityColumns(),
+	}
+}
+
+func tableGitHubOrganizationExternalIdentityList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	quals := d.EqualsQuals
+	org := quals["organization"].GetStringValue()
+
+	pageSize := adjustPageSize(100, d.QueryContext.Limit)
+
+	var query struct {
+		RateLimit    models.RateLimit
+		Organization struct {
+			SamlIdentityProvider struct {
+				ExternalIdentities struct {
+					PageInfo   models.PageInfo
+					TotalCount int
+					Nodes      []models.OrganizationExternalIdentity
+				} `graphql:"externalIdentities(first: $pageSize, after: $cursor)"`
+			}
+		} `graphql:"organization(login: $login)"`
+	}
+
+	variables := map[string]interface{}{
+		"login":    githubv4.String(org),
+		"pageSize": githubv4.Int(pageSize),
+		"cursor":   (*githubv4.String)(nil),
+	}
+
+	client := connectV4(ctx, d)
+
+	listPage := func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+		return nil, client.Query(ctx, &query, variables)
+	}
+
+	for {
+		_, err := plugin.RetryHydrate(ctx, d, h, listPage, retryConfig())
+		plugin.Logger(ctx).Debug(rateLimitLogString("github_pull_request_comment", &query.RateLimit))
+		if err != nil {
+			plugin.Logger(ctx).Error("github_pull_request_comment", "api_error", err)
+			return nil, err
+		}
+
+		for _, eid := range query.Organization.SamlIdentityProvider.ExternalIdentities.Nodes {
+			d.StreamListItem(ctx, eid)
+
+			// Context can be cancelled due to manual cancellation or the limit has been hit
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
+		}
+
+		if !query.Organization.SamlIdentityProvider.ExternalIdentities.PageInfo.HasNextPage {
+			break
+		}
+		variables["cursor"] = githubv4.NewString(query.Organization.SamlIdentityProvider.ExternalIdentities.PageInfo.EndCursor)
+	}
+
+	return nil, nil
+}
