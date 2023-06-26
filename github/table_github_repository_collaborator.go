ff722f79e45c0b908b58b7548e8eac838cb06822
Graza <code@graza.io>
2023-06-26 09:02:15 +0100

    feat: filtering for repository_collaborators table

--- github/table_github_repository_collaborator.go
+++ github/table_github_repository_collaborator.go
@@ -2,16 +2,19 @@ package github
 
 import (
 	"context"
+	"fmt"
 	"github.com/shurcooL/githubv4"
 	"github.com/turbot/steampipe-plugin-github/github/models"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+	"strings"
 )
 
 func gitHubRepositoryCollaboratorColumns() []*plugin.Column {
 	return []*plugin.Column{
 		{Name: "repository_full_name", Type: proto.ColumnType_STRING, Description: "The full name of the repository, including the owner and repo name.", Transform: transform.FromQual("repository_full_name")},
+		{Name: "filter", Type: proto.ColumnType_STRING, Description: "Affiliation filter - valid values 'ALL' (default), 'OUTSIDE', 'DIRECT'.", Transform: transform.FromQual("filter"), Default: "ALL"},
 		{Name: "permission", Type: proto.ColumnType_STRING, Description: "The permission the collaborator has on the repository."},
 		{Name: "user_login", Type: proto.ColumnType_STRING, Description: "The login of the collaborator", Transform: transform.FromField("Node.Login")},
 	}
@@ -24,7 +27,18 @@ func tableGitHubRepositoryCollaborator() *plugin.Table {
 		List: &plugin.ListConfig{
 			Hydrate:           tableGitHubRepositoryCollaboratorList,
 			ShouldIgnoreError: isNotFoundError([]string{"404"}),
-			KeyColumns:        plugin.SingleColumn("repository_full_name"),
+			KeyColumns: []*plugin.KeyColumn{
+				{
+					Name:    "repository_full_name",
+					Require: plugin.Required,
+				},
+				{
+					Name:       "filter",
+					Require:    plugin.Optional,
+					Operators:  []string{"="},
+					CacheMatch: "exact",
+				},
+			},
 		},
 		Columns: gitHubRepositoryCollaboratorColumns(),
 	}
@@ -34,6 +48,21 @@ func tableGitHubRepositoryCollaboratorList(ctx context.Context, d *plugin.QueryD
 	quals := d.EqualsQuals
 	fullName := quals["repository_full_name"].GetStringValue()
 	owner, repoName := parseRepoFullName(fullName)
+	filter := quals["filter"].GetStringValue()
+	affiliation := githubv4.CollaboratorAffiliationAll
+
+	if filter != "" {
+		switch strings.ToLower(filter) {
+		case "direct":
+			affiliation = githubv4.CollaboratorAffiliationDirect
+		case "outside":
+			affiliation = githubv4.CollaboratorAffiliationOutside
+		case "all":
+			affiliation = githubv4.CollaboratorAffiliationAll
+		default:
+			return nil, fmt.Errorf("filter must be 'ALL', 'OUTSIDE' or 'DIRECT' you provided '%s' which is invalid", filter)
+		}
+	}
 
 	var query struct {
 		RateLimit  models.RateLimit
@@ -45,16 +74,17 @@ func tableGitHubRepositoryCollaboratorList(ctx context.Context, d *plugin.QueryD
 					Permission githubv4.RepositoryPermission
 					Node       models.BasicUser
 				}
-			} `graphql:"collaborators(first: $pageSize, after: $cursor)"`
+			} `graphql:"collaborators(first: $pageSize, after: $cursor, affiliation: $affiliation)"`
 		} `graphql:"repository(owner: $owner, name: $repo)"`
 	}
 
 	pageSize := adjustPageSize(100, d.QueryContext.Limit)
 	variables := map[string]interface{}{
-		"owner":    githubv4.String(owner),
-		"repo":     githubv4.String(repoName),
-		"pageSize": githubv4.Int(pageSize),
-		"cursor":   (*githubv4.String)(nil),
+		"owner":       githubv4.String(owner),
+		"repo":        githubv4.String(repoName),
+		"pageSize":    githubv4.Int(pageSize),
+		"cursor":      (*githubv4.String)(nil),
+		"affiliation": affiliation,
 	}
 
 	client := connectV4(ctx, d)
@@ -66,6 +96,9 @@ func tableGitHubRepositoryCollaboratorList(ctx context.Context, d *plugin.QueryD
 		_, err := plugin.RetryHydrate(ctx, d, h, listPage, retryConfig())
 		plugin.Logger(ctx).Debug(rateLimitLogString("github_repository_collaborator", &query.RateLimit))
 		if err != nil {
+			if strings.Contains(err.Error(), "You do not have permission to view repository collaborators") {
+				return nil, nil
+			}
 			plugin.Logger(ctx).Error("github_repository_collaborator", "api_error", err)
 			return nil, err
 		}
