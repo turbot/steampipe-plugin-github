67c4d526511d451fb353d74aa43bbb63d29fd573
Graza <code@graza.io>
2023-07-18 19:43:19 +0100

    refactor: adjust column description
    Co-authored-by: cbruno10 <cody@turbot.com>

--- github/table_github_issue.go
+++ github/table_github_issue.go
@@ -53,7 +53,7 @@ func sharedIssueColumns() []*plugin.Column {
 		{Name: "comments_total_count", Type: proto.ColumnType_INT, Transform: transform.FromField("Comments.TotalCount", "Node.Comments.TotalCount"), Description: "Count of comments on the issue."},
 		{Name: "labels_total_count", Type: proto.ColumnType_INT, Transform: transform.FromField("Labels.TotalCount", "Node.Labels.TotalCount"), Description: "Count of labels on the issue."},
 		{Name: "labels_src", Type: proto.ColumnType_JSON, Transform: transform.FromField("Labels.Nodes", "Node.Labels.Nodes"), Description: "The first 100 labels associated to the issue."},
-		{Name: "labels", Type: proto.ColumnType_JSON, Description: "A map of label names associated with this issue, in Steampipe standard format.", Transform: transform.From(getIssueLabels)},
+		{Name: "labels", Type: proto.ColumnType_JSON, Description: "A map of labels for the issue.", Transform: transform.From(getIssueLabels)},
 		{Name: "user_can_close", Type: proto.ColumnType_BOOL, Transform: transform.FromField("UserCanClose", "Node.UserCanClose"), Description: "If true, user can close the issue."},
 		{Name: "user_can_react", Type: proto.ColumnType_BOOL, Transform: transform.FromField("UserCanReact", "Node.UserCanReact"), Description: "If true, user can react on the issue."},
 		{Name: "user_can_reopen", Type: proto.ColumnType_BOOL, Transform: transform.FromField("UserCanReopen", "Node.UserCanReopen"), Description: "If true, user can reopen the issue."},
