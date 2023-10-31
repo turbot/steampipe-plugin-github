167d723b52562c8b75e205e1472db9a2dbe7e297
sourav chakraborty <sourav@turbot.com>
2023-10-31 20:22:51 +0530

    Update github_team and github_my_team table

--- github/table_github_my_team.go
+++ github/table_github_my_team.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"context"
+
 	"github.com/shurcooL/githubv4"
 	"github.com/turbot/steampipe-plugin-github/github/models"
 
@@ -24,7 +25,7 @@ func tableGitHubMyTeam() *plugin.Table {
 	}
 }
 
-func tableGitHubMyTeamList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+func tableGitHubMyTeamList(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	var query struct {
 		RateLimit models.RateLimit
 		Viewer    struct {
@@ -49,6 +50,7 @@ func tableGitHubMyTeamList(ctx context.Context, d *plugin.QueryData, h *plugin.H
 		"pageSize":    githubv4.Int(pageSize),
 		"cursor":      (*githubv4.String)(nil),
 	}
+	appendTeamColumnIncludes(&variables, d.QueryContext.Columns)
 
 	client := connectV4(ctx, d)
 
@@ -68,7 +70,7 @@ func tableGitHubMyTeamList(ctx context.Context, d *plugin.QueryData, h *plugin.H
 				break
 			}
 			if org.Teams.PageInfo.HasNextPage {
-				ts, err := getAdditionalTeams(ctx, client, org.Login, org.Teams.PageInfo.EndCursor)
+				ts, err := getAdditionalTeams(ctx, d, client, org.Login, org.Teams.PageInfo.EndCursor)
 				if err != nil {
 					plugin.Logger(ctx).Error("github_my_team", "api_error", err)
 					return nil, err
@@ -98,7 +100,7 @@ func tableGitHubMyTeamList(ctx context.Context, d *plugin.QueryData, h *plugin.H
 	return nil, nil
 }
 
-func getAdditionalTeams(ctx context.Context, client *githubv4.Client, org string, initialCursor githubv4.String) ([]models.TeamWithCounts, error) {
+func getAdditionalTeams(ctx context.Context, d *plugin.QueryData, client *githubv4.Client, org string, initialCursor githubv4.String) ([]models.TeamWithCounts, error) {
 	var query struct {
 		RateLimit    models.RateLimit
 		Organization struct {
@@ -114,6 +116,7 @@ func getAdditionalTeams(ctx context.Context, client *githubv4.Client, org string
 		"cursor":   githubv4.NewString(initialCursor),
 		"login":    githubv4.String(org),
 	}
+	appendTeamColumnIncludes(&variables, d.QueryContext.Columns)
 
 	var ts []models.TeamWithCounts
 	for {
