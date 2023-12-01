7bc25913534ef643e2b288624dac63713f9654fe
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2023-12-01 11:21:28 +0530

    Update github_rate_limit table (#368)
    Co-authored-by: Madhushree Ray <madhushree@turbot.com>

--- github/models/rate_limit.go
+++ github/models/rate_limit.go
@@ -4,10 +4,10 @@ import "time"
 
 // RateLimit information
 type RateLimit struct {
-	Remaining int
-	Used      int
-	Cost      int
-	Limit     int
-	ResetAt   time.Time
-	NodeCount int
+	Remaining int       `graphql:"remaining @include(if:$includeRLRemaining)" json:"remaining"`
+	Used      int       `graphql:"used @include(if:$includeRLUsed)" json:"used"`
+	Cost      int       `graphql:"cost @include(if:$includeRLCost)" json:"cost"`
+	Limit     int       `graphql:"limit @include(if:$includeRLLimit)" json:"limit"`
+	ResetAt   time.Time `graphql:"resetAt @include(if:$includeRLResetAt)" json:"reset_at"`
+	NodeCount int       `graphql:"remaining @include(if:$includeRLNodeCount)" json:"node_count"`
 }
